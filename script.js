66307d9d30bc46d7d0fd5be63a6f5d9d85904bb6
Dan <dan.belfield@outlook.com>
2019-08-29 22:56:13 +0100

    Value now being passed from button selection to container

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 
-const btn = document.getElementById('btn');
 const container = document.querySelector('.container');
 
-btn.addEventListener('click', generateTable);
+document.addEventListener('click', buttonClick);
 
-function generateTable() {
-     
-    let value = 20;
+function buttonClick(e) {
+    if (e.target.classList.contains('btn')) {
+        let value = e.target.value;
+        generateTable(value);
+    }
+}
 
-    for (let i = 0; i < value; i++){
-        let row = document.createElement('div');
-        row.className = 'row';
-            for (let x = 1; x <= value; x++){
-                let cell = document.createElement('div');
-                cell.className = 'cell';
-                row.appendChild(cell);
-        container.appendChild(row);
+function generateTable(value) {
+        for (let i = 0; i < value; i++){
+            let row = document.createElement('div');
+            row.className = 'row';
+                for (let x = 1; x <= value; x++){
+                    let cell = document.createElement('div');
+                    cell.className = 'cell';
+                    row.appendChild(cell);
+            container.appendChild(row);
         }
     
     }
@@ -24,7 +27,6 @@ function generateTable() {
     let containerHeight = (value + 1) * 11;             // move to start?
     container.style.height = containerHeight + 'px';
     container.style.width = containerWidth + 'px';
-
     container.style.marginLeft = '100px'; // once getting values in for grid sizes, customise margins.
 
 }
