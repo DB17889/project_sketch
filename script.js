87eaf3977324da897e5d6a4d7d2f8cfe41a6c190
Dan <dan.belfield@outlook.com>
2019-08-31 20:19:28 +0100

    container center positioning, margins and borders are spot on

--- script.js
+++ script.js
@@ -11,25 +11,26 @@ function buttonClick(e) {
 }
 
 function generateTable(value) {
-        for (let i = 0; i < value; i++){
-            let row = document.createElement('div');
-            row.className = 'row';
-                for (let x = 1; x <= value; x++){
-                    let cell = document.createElement('div');
-                    cell.className = 'cell';
-                    row.appendChild(cell);
-            container.appendChild(row);
-        }
-    
-    }
 
     let containerWidth = value * 12;                    // move to start?
     let containerHeight = value * 11;             // move to start?
+
+    for (let i = 0; i < value; i++){
+        let row = document.createElement('div');
+        row.className = 'row';
+            for (let x = 1; x <= value; x++){
+                let cell = document.createElement('div');
+                cell.className = 'cell';
+                row.appendChild(cell);
+                container.appendChild(row);
+            }
+
+    }
+
     container.style.border = 'solid black 3px';
     container.style.height = containerHeight + 'px';
     container.style.width = containerWidth + 'px';
-    container.style.marginLeft = '100px'; // once getting values in for grid sizes, customise margins.
-
+    
 }
     
 container.addEventListener("mouseover", function(e) {
