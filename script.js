17dfa07b5d3f83fc0e8c0ae02078149f79459895
Dan <dan.belfield@outlook.com>
2019-09-05 22:34:15 +0100

    GOT THE COLOR SELECTOR BUTTONS WORKING!!!

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const container = document.querySelector('.container');
 const body = document.querySelector('body');
 const bodyContainer = document.querySelector('.bodyContainer');
 let buttonDiv = document.getElementById('buttons');
-//let selectedColor = 'white';
 
 document.addEventListener('click', buttonClick);
 document.addEventListener('click', resetClick);
-//document.addEventListener('click', colorClick);
+
 
 function buttonClick(e) {
     if (e.target.classList.contains('btn')) {
@@ -44,38 +43,34 @@ function generateTable(value) {
  
 function createElements() {
 
-    /*let colorDiv = document.createElement('div');
+    let colorDiv = document.createElement('div');
     colorDiv.className = 'colorDiv';
     colorDiv.id = 'colorDiv';
     body.appendChild(colorDiv);
 
     let colorBlue = document.createElement('button');
     colorBlue.className = 'colorBtn';
-    colorBlue.id = 'blueBtn';
     colorBlue.value = 'blue';
     colorBlue.style.backgroundColor = 'blue';
     colorDiv.appendChild(colorBlue);
 
     let colorRed = document.createElement('button');
     colorRed.className = 'colorBtn';
-    colorRed.id = 'redBtn';
     colorRed.value = 'red';
     colorRed.style.backgroundColor = 'red';
     colorDiv.appendChild(colorRed);
 
     let colorGreen = document.createElement('button');
     colorGreen.className = 'colorBtn';
-    colorGreen.id = 'greenBtn';
     colorGreen.value = 'green';
     colorGreen.style.backgroundColor = 'green';
     colorDiv.appendChild(colorGreen);
 
     let colorYellow = document.createElement('button');
     colorYellow.className = 'colorBtn';
-    colorYellow.id = 'yellowBtn';
     colorYellow.value = 'yellow';
     colorYellow.style.backgroundColor = 'yellow';
-    colorDiv.appendChild(colorYellow);*/
+    colorDiv.appendChild(colorYellow);
     
     let resetDiv = document.createElement('div');
     resetDiv.className = 'resetDiv';
@@ -90,29 +85,67 @@ function createElements() {
 }
 
 
-/*function colorClick(e) {
-    if (e.target.classList.contains('colorBtn')) {
-        let selectedColor = e.target.value;
-        return selectedColor;
+let selectedColor = 'black';
+
+document.addEventListener('click', colorClick);
+
+function colorClick(e) {    
+    if (e.target.classList.contains('colorBtn')) {  
+        selectedColor = e.target.value;
+        console.log('logged: ' + selectedColor);    
+    }
+    switch (selectedColor) {
+        case 'blue':
+            container.addEventListener("mouseover", function(e) {
+                if(e.target.classList.contains('cell')){
+                    e.target.style.backgroundColor = "blue";
+                }   
+            })
+            break;
+        case 'red':
+            container.addEventListener("mouseover", function(e) {
+                if(e.target.classList.contains('cell')){
+                    e.target.style.backgroundColor = "red";
+                }   
+            })
+            break;
+        case 'yellow':
+                container.addEventListener("mouseover", function(e) {
+                    if(e.target.classList.contains('cell')){
+                        e.target.style.backgroundColor = "yellow";
+                    }   
+                })
+                break;
+        case 'green':
+                container.addEventListener("mouseover", function(e) {
+                    if(e.target.classList.contains('cell')){
+                        e.target.style.backgroundColor = "green";
+                    }   
+                })
+                break;
+        default:
+                container.addEventListener("mouseover", function(e) {
+                    if(e.target.classList.contains('cell')){
+                        e.target.style.backgroundColor = "black";
+                    }   
+                })
     }
 }
 
-console.log(selectedColor);*/
 
+/*
 container.addEventListener("mouseover", function(e) {
     if(e.target.classList.contains('cell')){
         e.target.style.backgroundColor = "red";
     }   
 })
-
+*/
 
 function resetClick(e) {
     if (e.target.classList.contains('resetButton')) {
         location.reload(true);
     }
 }
-
-
 
 
 
